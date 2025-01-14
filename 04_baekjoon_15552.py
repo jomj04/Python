ec108001014489fe5073411396f8af2217f1bfb1
MinJun Jo <162237931+jomj04@users.noreply.github.com>
2025-01-15 01:08:48 +0900

    Delete 04_baekjoon_15552.py

--- 04_baekjoon_15552.py
+++
@@ -1,9 +0,0 @@
-# 백준 15552번 문제 (빠른 입출력 관련)
-import sys
-T = int(sys.stdin.readline().rstrip())
-result = []
-for x in range(T):
-    nums = map(int, sys.stdin.readline().rstrip().split())
-    result.append(sum(nums))
-for x in result:
-    print(x, end = ' ')
